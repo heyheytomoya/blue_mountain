b792e6109d7b9ac31817e85d459c309224700f4e
tomoyayamazaki <tomoya.yamazaki.cb@gmail.com>
2023-12-23 23:12:17 +0900

    再生順に修正

--- fetch_cc.py
+++ fetch_cc.py
@@ -2,11 +2,13 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import googleapiclient.discovery
 import googleapiclient.errors
 from datetime import datetime, timedelta
-#APIキーを設定します
+
+# APIキーを設定します
 api_key = "googleのAPI"
 youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
 channel_id = "UCmMNBtjt6_AjKwC2p0aN8wg"  # チャンネルIDを設定します
 from_date = datetime(2023, 11, 20)  # 字幕を取得したい動画の最小投稿日を設定します
+
 def get_channel_videos(channel_id, from_date):
     # 指定したチャンネルから動画のリストを取得します
     res = youtube.channels().list(id=channel_id, part='contentDetails').execute()
@@ -22,8 +24,11 @@ def get_channel_videos(channel_id, from_date):
         next_page_token = res.get('nextPageToken')
         if next_page_token is None:
             break
+    # 動画を再生数でソートします（再生数の多い順）
+    videos.sort(key=lambda x: int(x['snippet']['statistics']['viewCount']), reverse=True)
     return [video for video in videos if
             datetime.strptime(video['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ") > from_date]
+
 videos = get_channel_videos(channel_id, from_date)
 for video in videos:
     video_id = video['snippet']['resourceId']['videoId']
